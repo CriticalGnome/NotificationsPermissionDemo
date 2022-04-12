23aa4c7db535bf94d90c39c06e15aa648148e21f
Sergey Kalinovsky <lord.skiminok@gmail.com>
2022-04-12 13:14:28 +0200

    Refactored to use permission request

--- app/src/main/java/com/criticalgnome/notificationspermissiondemo/MainActivity.kt
+++ app/src/main/java/com/criticalgnome/notificationspermissiondemo/MainActivity.kt
@@ -4,13 +4,21 @@ import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.criticalgnome.notificationspermissiondemo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +28,50 @@ class MainActivity : AppCompatActivity() {
         getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted. Continue the action or workflow.
+            // TODO send notification
+        } else {
+            // Explain to the user that the feature is unavailable because the
+            // features requires a permission that the user has denied. At the
+            // same time, respect the user's decision. Don't link to system
+            // settings in an effort to convince the user to change their
+            // decision.
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val b1 = shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)
+//        when {
+//            ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED -> {
+//                Log.e("NPD", "onCreate: PERMISSION GRANTED")
+//                // TODO send notification
+//            }
+//            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+//                Snackbar.make(
+//                    binding.root,
+//                    "Notification blocked",
+//                    Snackbar.LENGTH_LONG
+//                ).setAction("Settings") {
+//                    // Responds to click on the action
+//                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                    val uri: Uri = Uri.fromParts("package", packageName, null)
+//                    intent.data = uri
+//                    startActivity(intent)
+//                }.show()
+//            }
+//            else -> {
+//                // The registered ActivityResultCallback gets the result of this request
+//                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+//            }
+//        }
 
         createChannel(CHANNEL_1_ID, CHANNEL_1_NAME, CHANNEL_1_DESCRIPTION)
         createChannel(CHANNEL_2_ID, CHANNEL_2_NAME, CHANNEL_2_DESCRIPTION)
@@ -66,18 +112,14 @@ class MainActivity : AppCompatActivity() {
         message: String,
         priority: Int = NotificationCompat.PRIORITY_DEFAULT
     ) = with(NotificationManagerCompat.from(this)) {
-        if (areNotificationsEnabled()) {
-            notify(
-                notificationId,
-                NotificationCompat.Builder(this@sendNotification, channelId)
-                    .setSmallIcon(iconId)
-                    .setContentTitle(title)
-                    .setContentText(message)
-                    .setPriority(priority)
-                    .build())
-        } else {
-//            requestPermissions(Manifest.permission.POST_NOTIFICATIONS)
-        }
+        notify(
+            notificationId,
+            NotificationCompat.Builder(this@sendNotification, channelId)
+                .setSmallIcon(iconId)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(priority)
+                .build())
     }
 
     companion object {
